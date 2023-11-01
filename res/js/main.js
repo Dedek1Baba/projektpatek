bf402d89995bf7c82552c0f2c2ad43387a85ac6b
Dedek1Baba <jozef.tomko@gmail.com>
2023-11-01 17:58:42 +0100

    ZMENY

--- res/js/main.js
+++ res/js/main.js
@@ -2,96 +2,84 @@ const jordan4 = document.getElementById("jordan4").addEventListener("click", Cou
 const counter = document.getElementById("counter");
 const upgrade1 = document.getElementById("upgrade1");
 const upgrade2 = document.getElementById("upgrade2");
-const upgrade3 = document.getElementById("upgrade3");
-const upgrade4 = document.getElementById("upgrade4");
+const clicker1 = document.getElementById("clicker1");
+const clicker2 = document.getElementById("clicker2");
  
-// Počet jordanu
 let NumberOfJordan = 0;
- 
-// Cena vylepšení
-let UpgradeCost = 25;
-let UpgradeCost2 = 100;
- 
-// Vylepšené počítání
+let UpgradePrice = 25;
+let UpgradePrice2 = 200;
 let UpgradedCount = 1;
+let AutoclickerPrice = 400;
+let AutoclickerPrice2 = 1000;
+let AutoClickerIncrease = 0;
  
-// Cena autoclickeru
-let AutoclickCost = 400;
-let AutoclickCost2 = 800;
-let AutoclickCost3 = 1600;
+upgrade1.innerText = "Buy Cleaner: " + UpgradePrice;
+upgrade1.addEventListener("click", Upgrade1);
  
-// Automatické navyšování
-let AutoclickIncrease = 0;
+upgrade2.innerText = "Buy Laces: " + UpgradePrice2;
+upgrade2.addEventListener("click", Upgrade2);
  
-upgrade1.innerText = "Buy Cleaner: " + UpgradeCost;
-upgrade1.addEventListener("click", Upgrader1);
+clicker1.innerText = "Buy Box Farm: " + AutoclickerPrice;
+clicker1.addEventListener("click", AutoClickerUpgrade1);
  
-upgrade2.innerText = "Buy New Laces: " + UpgradeCost2;
-upgrade2.addEventListener("click", Upgrader2);
- 
-upgrade3.innerText = "Buy Box Farm: " + AutoclickCost;
-upgrade3.addEventListener("click", AutoclickUpgrade1);
- 
-upgrade4.innerText = "Buy Jordan Farm: " + AutoclickCost2;
-upgrade4.addEventListener("click", AutoclickUpgrade2);
- 
+clicker2.innerText = "Buy SNKR Farm: " + AutoclickerPrice2;
+clicker2.addEventListener("click", AutoClickerUpgrade2);
  
 function Count() {
     NumberOfJordan += UpgradedCount;
     counter.innerText = "Jordan: " + NumberOfJordan;
 }
  
-function Upgrader1() {
-    if (NumberOfJordan >= UpgradeCost) {
-        NumberOfJordan -= UpgradeCost;
-        UpgradeCost *= 2;
+function Upgrade1() {
+    if (NumberOfJordan >= UpgradePrice) {
+        NumberOfJordan -= UpgradePrice;
+        UpgradePrice *= 2;
         UpgradedCount += 5;
         counter.innerText = "Jordan: " + NumberOfJordan;
-        upgrade1.innerText = "Upgrade Cleaner: " + UpgradeCost;
+        upgrade1.innerText = "Upgrade Cleaner: " + UpgradePrice;
     }
 }
  
-function Upgrader2() {
-    if (NumberOfJordan >= UpgradeCost) {
-        NumberOfJordan -= UpgradeCost;
-        UpgradeCost2 *= 2;
+function Upgrade2() {
+    if (NumberOfJordan >= UpgradePrice2) {
+        NumberOfJordan -= UpgradePrice2;
+        UpgradePrice2 *= 2;
         UpgradedCount += 10;
         counter.innerText = "Jordan: " + NumberOfJordan;
-        upgrade2.innerText = "New Laces: " + UpgradeCost2;
-    }
- 
-}
- 
-function AutoclickUpgrade1() {
-    if (NumberOfJordan >= AutoclickCost) {
-        NumberOfJordan -= AutoclickCost;
-        AutoclickCost *= 2;
-        counter.innerText = "Jordan: " + NumberOfJordan;
-        upgrade3.innerText = "Upgrade Box Farm: " + AutoclickCost;
- 
-        if(AutoclickIncrease === 0) {
-            setInterval(() => {
-                NumberOfJordan += AutoclickIncrease;
-                counter.innerText = "Jordan: " + NumberOfJordan;
-            }, 1000)
-        }
-        AutoclickIncrease += 1;
+        upgrade2.innerText = "Upgrade Laces: " + UpgradePrice2;
     }
 }
  
-function AutoclickUpgrade2() {
-    if (NumberOfJordan >= AutoclickCost2) {
-        NumberOfJordan -= AutoclickCost2;
-        AutoclickCost2 *= 2;
+function AutoClickerUpgrade1() {
+    if (NumberOfJordan >= AutoclickerPrice) {
+        NumberOfJordan -= AutoclickerPrice;
+        AutoclickerPrice *= 2;
         counter.innerText = "Jordan: " + NumberOfJordan;
-        upgrade4.innerText = "Upgrade Jordan Farm: " + AutoclickCost2;
+        clicker1.innerText = "Upgrade Box Farm: " + AutoclickerPrice;
  
-        if(AutoclickIncrease === 0) {
+        if(AutoClickerIncrease === 0) {
             setInterval(() => {
-                NumberOfJordan += AutoclickIncrease;
+                NumberOfJordan += AutoClickerIncrease;
                 counter.innerText = "Jordan: " + NumberOfJordan;
             }, 1000)
         }
-        AutoclickIncrease += 5;
+        AutoClickerIncrease += 1;
+    }
+}
+ 
+function AutoClickerUpgrade2() {
+    if (NumberOfJordan >= AutoclickerPrice2) {
+        NumberOfJordan -= AutoclickerPrice2;
+        AutoclickerPrice2 *= 2;
+        counter.innerText = "Jordan: " + NumberOfJordan;
+        clicker2.innerText = "Upgrade SNKR Farm: " + AutoclickerPrice2;
+ 
+        if(AutoClickerIncrease === 0) {
+            setInterval(() => {
+                NumberOfJordan += AutoClickerIncrease;
+                counter.innerText = "Jordan: " + NumberOfJordan;
+            }, 1000)
+        }
+        AutoClickerIncrease += 5;
     }
 }
